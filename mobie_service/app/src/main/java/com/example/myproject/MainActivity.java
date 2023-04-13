9878073b47d39d641cab4afd20268ef41d5a419a
phan huy nam <namphan.11092001@gmail.com>
2023-04-13 23:04:16 +0700

    mobie_service: init

---
+++ mobie_service/app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myproject.MainActivity2;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnDn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnDn = (Button)findViewById(R.id.buttonDn);
+
+        btnDn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ac2 = new Intent (MainActivity.this, MainActivity2.class);
+                startActivity(ac2);
+            }
+        });
+
+
+    }
+}
+
+
